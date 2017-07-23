0e21a700b2394fa6d8aed57e1ea77c2a3204380c
flpserrano <felipeserrano6@gmail.com>
2017-07-22 18:07:34 -0600

    First version to upload

---
+++ Bloques/Assets/Scripts/BallController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+	public Rigidbody rig;
+	public float initialVelocity = 600f;
+	bool onPlay = false;
+	Vector3 initialPosition;
+	public Transform bar;
+
+	void Awake()
+	{
+		initialPosition = transform.position;
+	}
+	void Update()
+	{
+		if(!onPlay && Input.GetButtonDown("Fire1"))
+		{
+			onPlay = true;
+			transform.SetParent(null);
+			rig.isKinematic = false;
+			rig.AddForce(new Vector3(initialVelocity,initialVelocity,0));
+		}
+	}
+
+	public void Reset()
+	{
+		transform.position = initialPosition;
+		transform.SetParent(bar);
+		onPlay = false;
+		stopMovement();
+	}
+
+	public void stopMovement()
+	{
+		rig.isKinematic = true;
+		rig.velocity = Vector3.zero;
+	}
+}
---
+++ Bloques/Assets/Scripts/BarController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarController : MonoBehaviour {
+
+	public float velocity = 20f;
+	Vector3 initialPosition;
+	
+	void Start () {
+		initialPosition = transform.position;
+	}
+
+	void Update () {
+		float horizontalKeyboard = Input.GetAxisRaw	("Horizontal");
+		float posX = transform.position.x + (horizontalKeyboard * velocity * Time.deltaTime);
+		transform.position = new Vector3(Mathf.Clamp(posX,-8,+8), transform.position.y, transform.position.z);
+	}
+
+	public void Reset()
+	{
+		transform.position = initialPosition;
+	}
+}
