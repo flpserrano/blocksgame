0c79ec51b60a2a7814e7bf16478a6dfdf3c4feff
flpserrano <felipeserrano6@gmail.com>
2017-07-24 08:40:26 -0600

    Video07

---
+++ Bloques/Assets/Scripts/Blocks.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blocks : MonoBehaviour {
+
+	public GameObject particles;
+	void OnCollisionEnter(Collision other)
+	{
+		Instantiate(particles,transform.position,Quaternion.identity);
+		Destroy(gameObject);	
+	}
+}
---
+++ Bloques/Assets/Scripts/Lifes.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lifes : MonoBehaviour {
+
+	public static int lifes = 3;
+	public Text lifeTxt;
+
+	public BallController ball;
+
+	public BarController bar;
+
+	void Start()
+	{
+		lifesUpdate();
+	}
+
+	public void lifeLost(){
+		lifes = lifes -1;
+		lifesUpdate();
+		bar.Reset();
+		ball.Reset();
+	}
+
+	void lifesUpdate(){
+		lifeTxt.text = "Lifes: "+Lifes.lifes;
+	}
+}
